e3cc7f18a457ac5b9e8b83ef6eb7d41379af7990
Johoho <joanne.j.ho@gmail.com>
2013-01-28 11:48:14 +1300

    home main photo @ 980 wide

---
+++ js/myscripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+$("#homeImgs").backstretch(["./img/home_img/image_1.jpg", "./img/home_img/image_2.jpg", "./img/home_img/image_3.jpg", "./img/home_img/image_4.jpg", "./img/home_img/image_5.jpg", "./img/home_img/image_6.jpg"], {
+		fade : 750,
+		duration : 50000
+	});
+
+});//end ready handler
